f311882d0f61b1cce054774246496a348784b610
Juan Quiroz <juan.quiroz1009@gmail.com>
2022-10-26 13:56:58 -0500

    express

---
+++ app.js
@@ -0,0 +1,8 @@
+'use strict'
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+//rutas
+
+//middelwares
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("ejecutado");
+'use strict'
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/portafolio')
+        .then(()=>{
+          console.log("base de datos cargada");
+        })
+        .catch(err => console.log(err));
